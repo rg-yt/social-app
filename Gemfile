5e402acca9b2bb56608c8c080be65d982079f666
rg-yt <103154533+rg-yt@users.noreply.github.com>
2025-12-03 16:11:59 -0500

    Add faker gem with seed data

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+
+gem "faker", "~> 3.5"
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+100.times do
+  Post.create(title: Faker::Book.title, body: Faker::Lorem.paragraph)
+end
